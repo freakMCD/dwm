5245b6319d5570b7bf5d75f8a93abf447bb89fe4
edwin <vastagox@gmail.com>
2023-02-18 21:52:55 -0500

    Update config.h

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "Main", "2", "", "4", "5", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -32,8 +32,8 @@ static const Rule rules[] = {
 
     { NULL, "URxvtFuzzy", NULL,  ~0,     1,   -1 },
     { NULL, "trans", NULL, 0, 1, -1},
-    { NULL, "newsboat", NULL, 1 << 3, 0, -1 },
-    { NULL, "neomutt", NULL, 1 << 3, 0, -1 },
+    { NULL, "newsboat", NULL, 1 << 5, 1, -1 },
+    { NULL, "neomutt", NULL, 1 << 5, 1, -1 },
     { NULL, "ftwitch", NULL, 0, 1, -1},
 };
 
@@ -115,12 +115,9 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_4,                3)
 	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+    { MODKEY,                       XK_KP_Down,toggleview,     {.ui = 1 << 5} },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
