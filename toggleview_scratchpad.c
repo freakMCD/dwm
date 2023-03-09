2f20d71bc038add1d621d8f9a719488b350625da
edwin <vastagox@gmail.com>
2023-03-09 00:36:07 -0500

    Improve toggleview_scratchpad patch

--- toggleview_scratchpad.c
+++ toggleview_scratchpad.c
@@ -1,9 +1,11 @@
 static void toggleview_scratchpad(const Arg *arg) {
     Client *c;
-	for(c = selmon->clients; !(c->tags & arg->ui) && c->next; c = c->next) ;
-    if (ISVISIBLE(c)) 
-        c->issticky=0;
-    else 
-        c->issticky=1;
-    arrange(selmon);
+    for(c = selmon->clients; c->tags & arg->ui && c->next; c = c->next) 
+    {
+        if (ISVISIBLE(c)) 
+            c->issticky=0;
+        else
+            c->issticky=1;
+        arrange(selmon);
+    }
 }
